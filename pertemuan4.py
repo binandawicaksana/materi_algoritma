fa8739a67daed6771fd206584cf6059358d68725
binandasoraten <binandawicaksana@gmail.com>
2024-10-14 18:46:58 +0700

    Pertemuan 5

--- pertemuan4.py
+++ pertemuan4.py
@@ -1,10 +1,10 @@
-# # slide 3
+# slide 3
 # matriks = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # print(matriks[0][1])  # Output: 2
 # matriks[2][2] = 10
 # print(matriks)  # Output: [[1, 2, 3], [4, 5, 6], [7, 8, 10]]
 
-# # slide 4
+# slide 4
 # matriks1 = [[1, 2], [3, 4]]
 # matriks2 = [[5, 6], [7, 8]]
 
@@ -15,22 +15,48 @@
 # matriks1 = [[1, 2], [3, 4]]
 # matriks2 = [[5, 6], [7, 8]]
 
-# hasil = [[sum(a*b for a, b in zip(matriks1_row, matriks2_col)) for 
-#           matriks2_col in zip(*matriks2)] for matriks1_row in matriks1]
+# hasil = [[sum(a*b for a, b in zip(matriks1_row, matriks2_col)) 
+#           for matriks2_col in zip(*matriks2)] 
+#           for matriks1_row in matriks1]
+
+
+# hasil2 = 0
+# kalimatriks = []
+# for matriks1_row in matriks1:
+#     print(f"matriks1_row = {matriks1_row}")
+#     for matriks2_col in zip(*matriks2):
+#         print(f"matriks2_col = {matriks2_col}")
+#         for a, b in zip(matriks1_row, matriks2_col):
+#             print(f"a = {a}, b = {b}")
+#             hasil = a*b
+#             print(f"hasil = {hasil}")
+#             hasil2 = hasil2 + hasil
+#             print(f"hasil2 = {hasil2}")
+#         kalimatriks.append(hasil2)
+#         print(f"hasil kali matriks = {kalimatriks}")
+#         hasil2 = 0
+        
+
+
+
 # print(hasil)  # Output: [[19, 22], [43, 50]]
+# hasilzip = zip([1,2,3],"matriks")
+# print(list[hasilzip])
 
 # # slide 6
-# n = 3
+# n = 2
 # identitas = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
 # print(identitas)
-# # Output: [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+# Output: [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
 # # SLide 8
 
 # mahasiswa = {'nama': 'Andi', 'nim': '12345', 'jurusan': 'Informatika'}
+# print(mahasiswa)
 # print(mahasiswa['nama'])  # Output: Andi
 # mahasiswa['angkatan'] = 2023
-# del mahasiswa['nim']
+# # mahasiswa['nama']='BUDI'
+# # del mahasiswa['nim']
 # print(mahasiswa)
 # for kunci, nilai in mahasiswa.items():
 #     print(f"{kunci}: {nilai}")
@@ -39,6 +65,7 @@
 
 # mahasiswa = {'nama': 'Andi', 'data': {'nim': '12345', 'jurusan': 'Informatika'}}
 # print(mahasiswa['data']['nim'])  # Output: 12345
+# print(mahasiswa['data'])
 
 # # Slide 10
 
@@ -61,10 +88,32 @@ def tambah_mahasiswa(mahasiswa, nim, nama, jurusan):
 def hapus_mahasiswa(mahasiswa, nim):
     if nim in mahasiswa:
         del mahasiswa[nim]
+        # print (mahasiswa[nim])
+
+# def hapus_nama(mahasiswa, nama):
+#     for kunci, nilai in mahasiswa.items():
+#         # print (f"{kunci}:{nilai}")
+#         # print (f"nilai {nilai}")
+#         # print (f"kunci = '{kunci}'")
+#         # nim = f"'{kunci}'"
+#         for nilai2 in nilai.items():
+#             # print (f"nilai 2 = {nilai2}")
+#             if nama in nilai2:
+#                 del mahasiswa[f'{kunci}']
+#                 # print (f"nilai 21 = {mahasiswa}")
+#                 # print (f"nim = {nim}")
+#             break
+#         break
+
+
+        # if nama in {kunci}[nama]:
+        #     del mahasiswa[kunci]
 
 mahasiswa = {}
 tambah_mahasiswa(mahasiswa, '001', 'Andi', 'Informatika')
-tambah_mahasiswa(mahasiswa, '002', 'Budi', 'Sistem Informasi')
-hapus_mahasiswa(mahasiswa, '001')
+tambah_mahasiswa(mahasiswa, '002', 'BUDI', 'Sistem Informasi')
+print(mahasiswa)
+hapus_mahasiswa(mahasiswa,'002')
+# hapus_nama(mahasiswa,'Andi')
 print(mahasiswa)
 
---
+++ pertemuan5.py
@@ -0,0 +1,106 @@
+# Single Linked List
+# Slide 9
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+# # Slide 10
+# class SingleLinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def tambah_node(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#         else:
+#             current = self.head
+#             while current.next:
+#                 current = current.next
+#             current.next = new_node
+
+# # Slide 11
+# # Membuat linked list dan menambah elemen
+# linked_list = SingleLinkedList()
+# linked_list.tambah_node(10)
+# linked_list.tambah_node(20)
+# linked_list.tambah_node(30)
+
+# # Akses elemen pertama
+# print(linked_list.head.data)  # Output: 10
+
+# # Slide 12
+    # def print_list(self):
+    #     current = self.head
+    #     while current:
+    #         print(current.data, end=' - ')
+    #         current = current.next
+# print_list(linked_list) # Output: 10 20 30
+
+# # Slide 13
+    # def hapus_node(self, data):
+    #     current = self.head
+    #     if current and current.data == data:
+    #         self.head = current.next
+    #         return
+    #     prev = None
+    #     while current and current.data != data:
+    #         prev = current
+    #         current = current.next
+    #     if current is None:
+    #         return
+    #     prev.next = current.next
+
+# hapus_node(linked_list,20)
+# print_list(linked_list)
+
+# # Double Linked List
+# # Slide 15
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+
+# # Slide 16
+# class DoubleLinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def tambah_node(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#         else:
+#             current = self.head
+#             while current.next:
+#                 current = current.next
+#             current.next = new_node
+#             new_node.prev = current
+
+# # Slide 17
+#     def hapus_node(self, data):
+#         current = self.head
+#         while current and current.data != data:
+#             current = current.next
+#         if current is None:
+#             return
+#         if current.prev:
+#             current.prev.next = current.next
+#         if current.next:
+#             current.next.prev = current.prev
+#         if current == self.head:
+#             self.head = current.next
+
+# # Slide 18
+# # Membuat double linked list dan menambah elemen
+# dll = DoubleLinkedList()
+# dll.tambah_node(10)
+# dll.tambah_node(20)
+# dll.tambah_node(30)
+
+# # Akses elemen pertama dan terakhir
+# print(dll.head.data)  # Output: 10
+# print(dll.head.next.next.data)  # Output: 30
+
