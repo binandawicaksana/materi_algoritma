d2602afbe9a12e229d1ee1a9837abafd475ea709
binandasoraten <binandawicaksana@gmail.com>
2024-10-20 20:41:06 +0700

    Update Materi

--- pertemuan5.py
+++ pertemuan5.py
@@ -1,69 +1,69 @@
 # Single Linked List
 # Slide 9
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.next = None
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# # Slide 10
-# class SingleLinkedList:
-#     def __init__(self):
-#         self.head = None
+# Slide 10
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
 
-#     def tambah_node(self, data):
-#         new_node = Node(data)
-#         if self.head is None:
-#             self.head = new_node
-#         else:
-#             current = self.head
-#             while current.next:
-#                 current = current.next
-#             current.next = new_node
+    def tambah_node(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
 
-# # Slide 11
-# # Membuat linked list dan menambah elemen
-# linked_list = SingleLinkedList()
-# linked_list.tambah_node(10)
-# linked_list.tambah_node(20)
-# linked_list.tambah_node(30)
+# Slide 11
+# Membuat linked list dan menambah elemen
+linked_list = SingleLinkedList()
+linked_list.tambah_node(10)
+linked_list.tambah_node(20)
+linked_list.tambah_node(30)
 
-# # Akses elemen pertama
-# print(linked_list.head.data)  # Output: 10
+# Akses elemen pertama
+print(linked_list.head.data)  # Output: 10
 
-# # Slide 12
-    # def print_list(self):
-    #     current = self.head
-    #     while current:
-    #         print(current.data, end=' - ')
-    #         current = current.next
+# Slide 12
+def print_list(self):
+    current = self.head
+    while current:
+        print(current.data, end='')
+        current = current.next
 # print_list(linked_list) # Output: 10 20 30
 
-# # Slide 13
-    # def hapus_node(self, data):
-    #     current = self.head
-    #     if current and current.data == data:
-    #         self.head = current.next
-    #         return
-    #     prev = None
-    #     while current and current.data != data:
-    #         prev = current
-    #         current = current.next
-    #     if current is None:
-    #         return
-    #     prev.next = current.next
+# Slide 13
+def hapus_node(self, data):
+    current = self.head
+    if current and current.data == data:
+        self.head = current.next
+        return
+    prev = None
+    while current and current.data != data:
+        prev = current
+        current = current.next
+    if current is None:
+        return
+    prev.next = current.next
 
-# hapus_node(linked_list,20)
-# print_list(linked_list)
+hapus_node(linked_list,10)
+print_list(linked_list)
 
-# # Double Linked List
-# # Slide 15
+# Double Linked List
+# Slide 15
 # class Node:
 #     def __init__(self, data):
 #         self.data = data
 #         self.next = None
 #         self.prev = None
 
-# # Slide 16
+# # # Slide 16
 # class DoubleLinkedList:
 #     def __init__(self):
 #         self.head = None
@@ -102,5 +102,14 @@
 
 # # Akses elemen pertama dan terakhir
 # print(dll.head.data)  # Output: 10
-# print(dll.head.next.next.data)  # Output: 30
+# print(dll.head.next.prev.data)  # Output: 30
+# print(dll.head.prev) 
 
+# DoubleLinkedList.hapus_node(dll,20)
+
+# def print_list(self):
+#     current = self.head
+#     while current:
+#         print(current.data, end=' - ')
+#         current = current.next
+# print_list(dll) # Output: 10 20 30
--- pertemuan6.py
+++ pertemuan6.py
@@ -1,1 +1,65 @@
-# Slide 
+# Slide 7
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(0, n-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# # Contoh
+# data = [64, 34, 25, 12, 22, 11, 90]
+# bubble_sort(data)
+# print(data)
+
+# Slide 11
+
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         min_idx = i
+#         for j in range(i+1, n):
+#             if arr[j] < arr[min_idx]:
+#                 min_idx = j
+#         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# # Contoh
+# data = [64, 25, 12, 22, 11]
+# selection_sort(data)
+# print(data)
+
+
+# Slide 15
+# def merge_sort(arr):
+#     if len(arr) > 1:
+#         mid = len(arr) // 2
+#         L = arr[:mid]
+#         R = arr[mid:]
+
+#         merge_sort(L)
+#         merge_sort(R)
+
+#         i = j = k = 0
+
+#         while i < len(L) and j < len(R):
+#             if L[i] < R[j]:
+#                 arr[k] = L[i]
+#                 i += 1
+#             else:
+#                 arr[k] = R[j]
+#                 j += 1
+#             k += 1
+
+#         while i < len(L):
+#             arr[k] = L[i]
+#             i += 1
+#             k += 1
+
+#         while j < len(R):
+#             arr[k] = R[j]
+#             j += 1
+#             k += 1
+
+# # Contoh
+# data = [38, 27, 43, 3, 9, 82, 10]
+# merge_sort(data)
+# print(data)
